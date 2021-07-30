86666f75b16a2a2a2b4f8c216c2e38eade0224bb
neillycubed <neillybusiness3@gmail.com>
2021-07-30 08:12:13 -0700

    add styling to NavBar component

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <div className='navContainer'>
+        <div className='navContainer flex border-b border-gray-200 fixed top-0 inset-x-0 z-100 h-16 items-center'>
             
         </div>
     )
