207fcbc99e6611c339a8285aec7f7daa0b4116d4
neillycubed <neillybusiness3@gmail.com>
2021-08-07 10:55:09 -0700

    create Main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+require('./Main.css');
+
+const Main = () => {
+    return (
+        <div className="mainContainer">
+            
+        </div>
+    )
+}
+
+export default Main
