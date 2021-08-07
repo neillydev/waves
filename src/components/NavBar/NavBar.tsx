7dc023ceedacc779ba5e710268f25b77abbce24f
neillycubed <neillybusiness3@gmail.com>
2021-08-07 10:15:11 -0700

    add search bar to NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,16 +7,26 @@ function NavBar() {
     return (
         <div className='navContainer flex border-b border-gray-200 fixed top-0 inset-x-0 z-100 h-16 items-center'>
             <div className="w-full max-w-screen-xl relative mx-auto px-6">
-                <div className="flex items-center">
-                    <div className="xl:w-1/5 lg:w-1/4 pl-6 pr-6 lg:pr-8">
+                <div className="navHeaderContainer flex items-center justify-between">
+                    <div className="navTitleContainer">
                         <div className="flex items-center">
                             <a href="/" className="block lg:mr-4">
-                                <WaveSVG className='w-auto md:block h-10' />
+                                <WaveSVG className='replaceThis w-auto md:block h-9' />
                             </a>
                             <h1 className="navTitle text-3xl">
                                 waves
                             </h1>
                         </div>
+                    </div>
+                    <div className="navSearchContainer flex flex-grow min-w-0 lg:w-3/4 xl:w-4/5">
+                        <div className="w-full min-w-0 lg:px-6 xl:w-3/4 xl:px-12 relative">
+                            <form action="/search" className="transition-colors duration-100 ease-in-out text-gray-600 py-2 pr-4 pl-10 block w-full appearance-none leading-normal border border-transparent rounded-lg focus:outline-none text-left select-none truncate focus:bg-white focus:border-gray-300 bg-gray-200">
+                                <input placeholder="Search creators" type="search" className="w-full bg-transparent outline-none" />
+                            </form>
+                        </div>
+                    </div>
+                    <div className="navLoginContainer flex w-auto md:block h-9">
+
                     </div>
                 </div>
             </div>
