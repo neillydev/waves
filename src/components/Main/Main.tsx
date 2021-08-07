c7db87a8d445fe5f31d43ab44ad3a05435a3b380
neillycubed <neillybusiness3@gmail.com>
2021-08-07 11:41:57 -0700

    add styling to Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,8 +4,20 @@ require('./Main.css');
 
 const Main = () => {
     return (
-        <div className="mainContainer">
-            
+        <div className="mainContainer flex justify-between">
+            <div className="leftBarContainer">
+                <div className="leftScrollContainer">
+                    <div className="leftScrollWrapper">
+                        <div className="navWrapper flex flex-col">
+                            <a href="/" className="navItem">Trending</a>
+                            <a href="/" className="navItem">Following</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mainContentContainer">
+
+            </div>
         </div>
     )
 }
