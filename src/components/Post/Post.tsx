91cd804b8a35bca629702a043ca7f870cda041f1
neillycubed <neillybusiness3@gmail.com>
2021-08-10 19:00:45 -0700

    add Post properties to Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,10 +2,38 @@ import React from 'react';
 
 require('./Post.css');
 
-const Post = () => {
+type PostProps = {
+    author: string;
+    title: string;
+    creatorAvatarImg: string;
+    contentTitle: string;
+    contentDescription: string; 
+    mediaType: "video" | "image";
+};
+
+const Post = ({ author, title, creatorAvatarImg, contentTitle, contentDescription, mediaType }: PostProps) => {
     return (
         <div className="postContainer">
-            
+            <span className="postWrapper">
+                <div className="post">
+                    <a href="/" className="creatorAvatar">
+                        <span className="creatorAvatarImg">
+                            <img src={creatorAvatarImg} />
+                        </span>
+                    </a>
+                    <div className="content">
+                        <div className="contentAuthorTitle">
+
+                        </div>
+                        <div className="contentDescription">
+
+                        </div>
+                        <div className="mediaContainer">
+
+                        </div>
+                    </div>
+                </div>
+            </span>
         </div>
     )
 };
