dd9c68679950554eeeb31bf9edaa581c813a6f4f
neillycubed <neillybusiness3@gmail.com>
2021-08-11 00:27:45 -0700

    create login modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import Main from './components/Main/Main';
 import NavBar from './components/NavBar/NavBar';
 
@@ -7,12 +7,33 @@ import './styles.css';
 require('./App.css');
 
 const App = () => {
+    const [loginModalActive, setLoginModalActive] = useState(false);
+
+    const handleLoginBtn = () => {
+        setLoginModalActive(!loginModalActive);
+    };
+
     return (
         <div className="appContainer">
-            <NavBar />
+            <NavBar isLoggedIn={false} handleLoginBtn={handleLoginBtn} />
             <div className="mainWrapper">
                 <Main />
             </div>
+            {
+                loginModalActive ?
+                    <div className="loginModalContainer">
+                        <div className="loginModalWrapper">
+                            <div>
+                                <img className="loginModalClose">
+
+                                </img>
+                            </div>
+                        </div>
+                        <div className="loginModalMask" onClick={()=>handleLoginBtn()}></div>
+                    </div>
+                    :
+                    null
+            }
         </div>
     )
 }
