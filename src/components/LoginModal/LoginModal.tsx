ea087dde88196696cb6fa43d9bde85d802d282f6
neillycubed <neillybusiness3@gmail.com>
2021-08-13 13:48:02 -0700

    add styling to login modal

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -4,16 +4,34 @@ require('./LoginModal.css');
 
 type LoginModalProps = {
     handleLoginBtn: () => void;
+    isLogin?: boolean;
 };
 
-const LoginModal = ({ handleLoginBtn }: LoginModalProps) => {
+const LoginModal = ({ handleLoginBtn, isLogin = false }: LoginModalProps) => {
     return (
         <div className="loginModalContainer">
-            <div className="loginModalWrapper">
-                <div>
+            <div className="loginModalWrapper flex flex-col">
+                <div className="loginModalHeader w-full">
                     <img className="loginModalClose">
 
                     </img>
+                </div>
+                <div className="loginModalBody">
+                    <div className="loginBody">
+                        <div className="loginContainer">
+                            <div className="loginTitle">
+                                Sign up for Waves
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="loginModalFooter">
+                    <div className="loginModalFooterWrapper flex items-center justify-center">
+                        <div>
+                            Already registered?
+                        </div>
+                        <a href="/" className="isLoginLink">Log In</a>
+                    </div>
                 </div>
             </div>
             <div className="loginModalMask" onClick={() => handleLoginBtn()}></div>
