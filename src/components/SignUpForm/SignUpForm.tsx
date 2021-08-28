ace134964f29ed4debc39f5204b1ddf8b92b97f8
neillycubed <neillybusiness3@gmail.com>
2021-08-27 23:09:39 -0700

    add username sanitizing and birthday modal logic

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -7,15 +7,23 @@ const SignUpForm = () => {
     const days = 31;
     const years = new Date().getFullYear();
 
+    const alphanumeric = /^[0-9a-zA-Z]+$/;
+
     const [registerSuccessful, setRegisterSuccessful] = useState(false);
 
     const [monthSelected, setMonthSelected] = useState(false);
     const [daySelected, setDaySelected] = useState(false);
     const [yearSelected, setYearSelected] = useState(false);
+    const [month, setMonth] = useState<string>();
+    const [day, setDay] = useState<string>();
+    const [year, setYear] = useState<string>();
 
     const [username, setUsername] = useState<string>();
+    const [usernameError, setUsernameError] = useState<string>('');
+
     const [email, setEmail] = useState<string>();
     const [emailExists, setEmailExists] = useState<boolean>(false);
+
     const [password, setPassword] = useState<string>();
     const [passwordConf, setPasswordConf] = useState<string>();
     const [matchingPass, setMatchingPass] = useState<boolean>(true);
@@ -25,9 +33,8 @@ const SignUpForm = () => {
         setDaySelected(false);
         setYearSelected(false);
     };
-    
+
     const onEmailInput = (event: React.FormEvent<HTMLInputElement>) => {
-        console.log(event.currentTarget.value);
         setEmail(event.currentTarget.value);
     };
 
@@ -46,6 +53,15 @@ const SignUpForm = () => {
 
     const handleRegister = (event: React.FormEvent<HTMLFormElement>) => { 
         event.preventDefault();
+        if(username && (username.length < 4 || username.length > 15)){
+            setUsernameError('Username must be between 4 and 15 characters');
+            return;
+        }
+        if(username && !username.match(alphanumeric)){
+            setUsernameError('Username can only contain letters and numbers');
+            return;
+        }
+        setUsernameError('');
         if(password !== passwordConf) {
             setMatchingPass(false);
             return;
@@ -79,36 +95,36 @@ const SignUpForm = () => {
                 </div>
                 <div className="birthdayContainer w-full flex justify-between">
                     <div className="birthdaySelectorContainer" onClick={() => setMonthSelected(true)}>
-                        <div className="birthdaySelector">
-                            Month
+                        <div className={`birthdaySelector ${month ? 'birthdaySelected' : null}`}>
+                            {month ? month : 'Month'}
                         </div>
                         {monthSelected ? <ul className="birthdayListContainer">
-                            {months.map(month => <li className="birthdayListItem">
+                            {months.map(month => <li className="birthdayListItem" onClick={(event) => { event.stopPropagation(); setMonth(month); handleBirthdayModal();}}>
                                 <span>{month}</span>
                             </li>)}
                         </ul> : null}
                     </div>
                     <div className="birthdaySelectorContainer" onClick={() => setDaySelected(true)}>
-                        <div className="birthdaySelector">
-                            Day
+                        <div className={`birthdaySelector ${day ? 'birthdaySelected' : null}`}>
+                            {day ? day : 'Day'}
                         </div>
                         {daySelected ? <ul className="birthdayListContainer">
-                            {[...Array(days)].map((value, i) => <li className="birthdayListItem"><span>{i+1}</span></li>)}
+                            {[...Array(days)].map((value, i) => <li className="birthdayListItem" onClick={(event) => { event.stopPropagation(); handleBirthdayModal(); setDay(`${i+1}`); }}><span>{i+1}</span></li>)}
                         </ul> : null}
                     </div>
                     <div className="birthdaySelectorContainer" onClick={() => setYearSelected(true)}>
-                        <div className="birthdaySelector">
-                            Year
+                        <div className={`birthdaySelector ${year ? 'birthdaySelected' : null}`}>
+                            {year ? year : 'Year'}
                         </div>
                         {yearSelected ? <ul className="birthdayListContainer">
-                            {[...Array(years)].map((value, i) => (years-i >= 1900) ? <li className="birthdayListItem"><span>{years-i}</span></li> : null)}
+                            {[...Array(years)].map((value, i) => (years-i >= 1900) ? <li className="birthdayListItem" onClick={(event) => { event.stopPropagation(); setYear(`${years-i}`); handleBirthdayModal(); }}><span>{years-i}</span></li> : null)}
                         </ul> : null}
                     </div>
                 </div>
                 <div className="loginTitleContainer">
                     Username
                 </div>
-                <div className="loginFormWrapper">
+                <div className={`loginFormWrapper flex flex-col ${usernameError.length > 0 ? 'loginFormError' : ''}`}>
                     <input 
                         type="text" 
                         placeholder="Username" 
@@ -145,8 +161,9 @@ const SignUpForm = () => {
                         onChange={onPasswordConfirm}
                         required
                     />
-                    {emailExists ? <h1 className="confirmPass">Email already registered</h1> : null}
-                    {matchingPass ? null : <h1 className="confirmPass">Passwords do not match</h1>}
+                    {emailExists ? <h1 className="signUpError">Email already registered</h1> : null}
+                    {matchingPass ? null : <h1 className="signUpError">Passwords do not match</h1>}
+                    {usernameError.length > 0 ? <h1 className="signUpError">{usernameError}</h1>: null}
                 </div>
                 <button type="submit" className="loginButton">
                     Next
