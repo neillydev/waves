52c0c299b895050d38db43a1a16b9f77f8006998
neillycubed <neillybusiness3@gmail.com>
2021-09-02 23:40:33 -0700

    add token verification handling to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useRef, useState, useReducer, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import LoginModal from './components/LoginModal/LoginModal';
 import Main from './components/Main/Main';
 import NavBar from './components/NavBar/NavBar';
+import { AuthContext, AuthProvider } from './components/contexts/AuthContext';
 import { ModalContext, ModalProvider } from './components/contexts/ModalContext';
 
 import './styles.css';
@@ -10,14 +11,43 @@ require('./App.css');
 
 
 const App = () => {
+    const { authState, authDispatch } = useContext(AuthContext);
     const { state } = useContext(ModalContext);
-    /* const handleLoginModal = () => {
-        setLoginModalActive(!loginModalActive);
-    }; */
+
+    const handleTokenVerify = (token: string) => {
+        fetch(`http://localhost:3000/auth`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ token: token })
+        })
+        .then(res => {
+            if (res.status == 200) {
+                authDispatch( { type: 'true' } );
+            }
+        })
+        .catch(error => {
+            authDispatch( { type: 'false' } );
+        });
+    };
+
+    const handleTokenCheck = () => {
+        let token = localStorage.getItem('token');
+
+        if (token) {
+            handleTokenVerify(token);
+        }
+        else{
+            console.error('No token found!')
+        }
+    };
+
+    useEffect(() => handleTokenCheck(), []);
 
     return (
         <div className="appContainer">
-            <NavBar isLoggedIn={false} />
+            <NavBar />
             <div className="mainWrapper">
                 <Main />
             </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalContext, ModalProvider } from './components/contexts/ModalContext';
+import { AuthContext, AuthProvider } from './components/contexts/AuthContext';
 
 
 ReactDOM.render(
     <BrowserRouter>
-        <ModalProvider>
-            <App />
-        </ModalProvider>
-    </BrowserRouter>, 
+        <AuthProvider>
+            <ModalProvider>
+                <App />
+            </ModalProvider>
+        </AuthProvider>
+    </BrowserRouter>,
     document.getElementById('root'));
