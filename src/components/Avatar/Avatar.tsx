25e88c7dd7e299586e6282eb12b907415573e664
neillycubed <neillybusiness3@gmail.com>
2021-09-03 15:33:04 -0700

    add dropdown hover event logic

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropdown from '../Dropdown/Dropdown';
 
 require('./Avatar.css');
 
 const Avatar = () => {
+    const [dropdownSelected, setDropdownSelected] = useState(false);
+
     return (
-        <div className="avatarContainer">
+        <div className="avatarContainer" onMouseEnter={(event) => {
+            setDropdownSelected(true);
+        }
+        }
+        onMouseLeave={(event) => {
+            setDropdownSelected(false)
+        }}>
             <span className="avatarWrapper">
                 <img src="https://avatars.githubusercontent.com/u/51303046?v=4" />
             </span>
+            {dropdownSelected ? <Dropdown /> : null}
         </div>
     )
 };
