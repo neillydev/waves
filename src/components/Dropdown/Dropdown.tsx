d2591b190dccb919543769061bb7215fce7e1c72
neillycubed <neillybusiness3@gmail.com>
2021-09-03 15:53:38 -0700

    add Dropdown list components and styling

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -10,6 +10,26 @@ const Dropdown = () => {
                     <span className="dropdownHeader">fooli</span>
                     <span className="dropdownSubheader">@kidfoolish</span>
                 </li>
+                <li className="dropdownItem px-1">
+                    <a href="/">
+                        Profile
+                    </a>
+                </li>
+                <li className="dropdownItem px-1">
+                    <a href="/">
+                        Inbox
+                    </a>
+                </li>
+                <li className="dropdownItem px-1">
+                    <a href="/">
+                        Settings
+                    </a>
+                </li>
+                <li className="dropdownItem px-1">
+                    <a href="/">
+                        Sign Out
+                    </a>
+                </li>
             </ul>
         </div>
     )
