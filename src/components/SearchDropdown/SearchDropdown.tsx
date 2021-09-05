6b2643773eef277dec0b9f614ca9cbd583b57237
neillycubed <neillybusiness3@gmail.com>
2021-09-05 08:15:58 -0700

    add user personalization to search bar dropdown

--- src/components/SearchDropdown/SearchDropdown.tsx
+++ src/components/SearchDropdown/SearchDropdown.tsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 require('./SearchDropdown.css');
 
 type SearchDropdownProps = {
-    username: string | undefined;
-    name: string | undefined;
-    avatar: string | undefined;
+    username: string | null;
+    name: string | null;
+    avatar: string | null;
 };
 
 const SearchDropdown = ({ username, name, avatar }: SearchDropdownProps) => {
@@ -16,15 +16,17 @@ const SearchDropdown = ({ username, name, avatar }: SearchDropdownProps) => {
                 Creators
             </div>
             <ul className="searchDropdownList">
+                {username ? 
                 <li className="searchDropdownItem">
                     <span className="searchItemAvatar">
-                        <img src={avatar} alt="" />
+                        <img src={avatar ? avatar : ""} alt="" />
                     </span>
                     <span className="searchItemContent">
                         <p className="searchItemUsername">{username}</p>
                         <p className="searchItemName">{name}</p>
                     </span>
                 </li>
+                : null}
             </ul>
         </div>
     )
