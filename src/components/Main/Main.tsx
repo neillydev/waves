3d85d4a9b5e87e8af30e485e56f6f9392624cc8c
neillycubed <neillybusiness3@gmail.com>
2021-09-05 08:15:28 -0700

    add empty post section header

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,6 +4,16 @@ import Post from '../Post/Post';
 
 require('./Main.css');
 
+/* <Post 
+author="kidfoolish" 
+title="Fooli" 
+creatorAvatarImg="https://avatars.githubusercontent.com/u/51303046?v=4" 
+contentTitle="" 
+contentDescription="1st post" 
+mediaType="image"
+mediaDescription="fooli"
+/> */
+
 const Main = () => {
     return (
         <div className="mainContainer flex justify-between">
@@ -34,7 +44,7 @@ const Main = () => {
                 </div>
             </div>
             <div className="mainContentContainer">
-                
+                <h4>Nothing to see here</h4>
             </div>
         </div>
     )
