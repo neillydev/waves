b0d5e897f272156472f3aee8855a29c68fca5cf5
neillycubed <neillybusiness3@gmail.com>
2021-09-11 00:48:26 -0700

    create Settings component for user settings

---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+require('./Settings.css');
+
+const Settings = () => {
+    return (
+        <div className="settingsContainer">
+
+        </div>
+    )
+};
+
+export default Settings;
