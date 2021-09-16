2eaad85e235b755d79995ac4b2b496576c3b8c26
neillycubed <neillybusiness3@gmail.com>
2021-09-16 05:08:34 -0700

    add profile fetching from API

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import BWWaveSVG from '../../svg/bw_wave.svg'; 
 
 require('./Profile.css');
 
 type ProfileProps = {
-
+    profileUsername: string;
 };
 
-const Profile = () => {
+type PostType = {
+    media: string;
+    caption: string;
+    likes: number;
+    date_posted: string;
+};
+
+type ProfileType = {
+    avatar: string;
+    username: string;
+    name: string;
+    followers: number;
+    following: number;
+    posts: PostType[];
+};
+
+const Profile = ({ profileUsername }: ProfileProps) => {
+
+    const [profile, setProfile] = useState<ProfileType>();
+
+    const [posts, setPosts] = useState<PostType[]>();
+
+    const handleFetchProfile = () => {
+        fetch(`http://localhost:3000/profile`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: profileUsername
+            })
+        })
+            .then(res => res.json())
+            .then((json: any) => {
+                setProfile(json);
+            })
+            .catch(error => console.error('Error: ' + error));
+    };
+
+    useEffect(() => {
+        console.log(profileUsername)
+        handleFetchProfile();
+    }, []);
+
     return (
         <div className="profileContainer">
             <div className="profileWrapper">
@@ -16,24 +61,24 @@ const Profile = () => {
                     </div>
                     <div className="profileDetails">
                         <div className="userControls">
-                            <h1>kidfoolish</h1>
+                            <h1>{profile?.username}</h1>
                             <div className="editProfile">
 
                             </div>
                         </div>
                         <ul className="userStats">
                             <li className="userFollowers">
-                                <b className="userStat">0</b> followers
+                                <b className="userStat">{profile?.followers}</b> followers
                             </li>
                             <li className="userFollowing">
-                                <b className="userStat">0</b> following
+                                <b className="userStat">{profile?.following}</b> following
                             </li>
                             <li className="userPosts">
-                                <b className="userStat">0</b> posts
+                                <b className="userStat">{profile?.posts.length}</b> posts
                             </li>
                         </ul>
                         <div className="userBio">
-                            <h1>fooli</h1>
+                            <h1>{profile?.name}</h1>
                             <h2>*+!:) !!🖤& * ^ 🌙* !+ ! * +</h2>
                         </div>
                     </div>
@@ -46,9 +91,36 @@ const Profile = () => {
                         <span>Tagged</span>
                     </li>
                 </ul>
+                <div className="postsContainer">
+                    <div className="postsFeed">
+                        <div className="postsItem">
+                            <div className="postsItemContent">
+                                <div className="postsItemCardWrapper">
+                                    <div className="postsItemCard">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="postsItemData">
+                                <div className="postsItemCaption">
+                                    <h3>we outchea frrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</h3>
+                                </div>
+                                <div className="postsItemViews">
+                                    <div className="viewsIcon">
+                                        <BWWaveSVG />
+                                        <h3>
+                                            729
+                                        </h3>
+                                    </div>
+                                    <h3>9-15-21</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default Profile;
