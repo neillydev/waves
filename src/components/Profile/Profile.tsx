f268b803f45beabdb21fb4d6d2bffc5e7a4fa101
neillycubed <neillybusiness3@gmail.com>
2021-09-16 01:32:34 -0700

    add profile stats for user profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -15,7 +15,26 @@ const Profile = () => {
                         <img src="https://avatars.githubusercontent.com/u/51303046?v=4" />
                     </div>
                     <div className="profileDetails">
+                        <div className="userControls">
+                            <h1>kidfoolish</h1>
+                            <div className="editProfile">
 
+                            </div>
+                        </div>
+                        <ul className="userStats">
+                            <li className="userFollowers">
+                                <b className="userStat">0</b> followers
+                            </li>
+                            <li className="userFollowing">
+                                <b className="userStat">0</b> following
+                            </li>
+                            <li className="userPosts">
+                                <b className="userStat">0</b> posts
+                            </li>
+                        </ul>
+                        <div className="userBio">
+
+                        </div>
                     </div>
                 </header>
             </div>
