4c1dbf0c5a867db319ae7864d8089545a867504b
neillycubed <neillybusiness3@gmail.com>
2021-09-21 00:09:31 -0700

    add input error checking for sign up form

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -17,12 +17,14 @@ const SignUpForm = () => {
     const [month, setMonth] = useState<string>();
     const [day, setDay] = useState<string>();
     const [year, setYear] = useState<string>();
+    const [birthdayError, setBirthdayError] = useState<string>('');
 
     const [username, setUsername] = useState<string>();
     const [usernameError, setUsernameError] = useState<string>('');
 
     const [email, setEmail] = useState<string>();
     const [emailExists, setEmailExists] = useState<boolean>(false);
+    const [emailInvalid, setEmailInvalid] = useState<boolean>(false);
 
     const [password, setPassword] = useState<string>();
     const [passwordConf, setPasswordConf] = useState<string>();
@@ -35,6 +37,8 @@ const SignUpForm = () => {
     };
 
     const onEmailInput = (event: React.FormEvent<HTMLInputElement>) => {
+        setEmailExists(false);
+        setEmailInvalid(false);
         setEmail(event.currentTarget.value);
     };
 
@@ -53,6 +57,10 @@ const SignUpForm = () => {
 
     const handleRegister = (event: React.FormEvent<HTMLFormElement>) => { 
         event.preventDefault();
+        if(!monthSelected || !daySelected || !yearSelected){
+            setBirthdayError('Birthday must be set');
+            return;
+        }
         if(username && (username.length < 4 || username.length > 15)){
             setUsernameError('Username must be between 4 and 15 characters');
             return;
@@ -62,6 +70,10 @@ const SignUpForm = () => {
             return;
         }
         setUsernameError('');
+        if(email && (!email.includes('@') || !email.includes('.'))){
+            setEmailInvalid(true);
+            return;
+        }
         if(password !== passwordConf) {
             setMatchingPass(false);
             return;
@@ -94,7 +106,7 @@ const SignUpForm = () => {
                     Birthday
                 </div>
                 <div className="birthdayContainer w-full flex justify-between">
-                    <div className="birthdaySelectorContainer" onClick={() => setMonthSelected(true)}>
+                    <div className={`birthdaySelectorContainer ${birthdayError ? 'loginFormError' : null}`} onClick={() => setMonthSelected(true)}>
                         <div className={`birthdaySelector ${month ? 'birthdaySelected' : null}`}>
                             {month ? month : 'Month'}
                         </div>
@@ -104,7 +116,7 @@ const SignUpForm = () => {
                             </li>)}
                         </ul> : null}
                     </div>
-                    <div className="birthdaySelectorContainer" onClick={() => setDaySelected(true)}>
+                    <div className={`birthdaySelectorContainer ${birthdayError ? 'loginFormError' : null}`} onClick={() => setDaySelected(true)}>
                         <div className={`birthdaySelector ${day ? 'birthdaySelected' : null}`}>
                             {day ? day : 'Day'}
                         </div>
@@ -112,7 +124,7 @@ const SignUpForm = () => {
                             {[...Array(days)].map((value, i) => <li className="birthdayListItem" onClick={(event) => { event.stopPropagation(); handleBirthdayModal(); setDay(`${i+1}`); }}><span>{i+1}</span></li>)}
                         </ul> : null}
                     </div>
-                    <div className="birthdaySelectorContainer" onClick={() => setYearSelected(true)}>
+                    <div className={`birthdaySelectorContainer ${birthdayError ? 'loginFormError' : null}`} onClick={() => setYearSelected(true)}>
                         <div className={`birthdaySelector ${year ? 'birthdaySelected' : null}`}>
                             {year ? year : 'Year'}
                         </div>
@@ -135,7 +147,7 @@ const SignUpForm = () => {
                 <div className="loginTitleContainer">
                     Email
                 </div>
-                <div className={`loginFormWrapper flex flex-col ${emailExists ? 'loginFormError' : ''}`}>
+                <div className={`loginFormWrapper flex flex-col ${emailExists || emailInvalid ? 'loginFormError' : ''}`}>
                     <input 
                         type="text" 
                         placeholder="Email" 
@@ -146,7 +158,7 @@ const SignUpForm = () => {
                 <div className="loginTitleContainer">
                     Password
                 </div>
-                <div className="loginFormWrapper">
+                <div className={`loginFormWrapper ${matchingPass ? '' : 'loginFormError'}`}>
                     <input 
                         type="text"
                         placeholder="Password"
@@ -161,6 +173,8 @@ const SignUpForm = () => {
                         onChange={onPasswordConfirm}
                         required
                     />
+                    {birthdayError.length > 0 ?  <h1 className="signUpError">{birthdayError}</h1> : null}
+                    {emailInvalid ? <h1 className="signUpError">Email not a valid email</h1> : null}
                     {emailExists ? <h1 className="signUpError">Email already registered</h1> : null}
                     {matchingPass ? null : <h1 className="signUpError">Passwords do not match</h1>}
                     {usernameError.length > 0 ? <h1 className="signUpError">{usernameError}</h1>: null}
