9df42c7cdbeda652d4ada26f31ea9906db3f743c
neillycubed <neillybusiness3@gmail.com>
2021-09-21 01:47:59 -0700

    create loading wave animation

---
+++ src/components/LoadingWave/LoadingWave.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+require('./LoadingWave.css');
+
+const LoadingWave = () => {
+    return (
+        <div className="loadingWave"><div></div><div></div><div></div></div>
+    )
+};
+
+export default LoadingWave;
