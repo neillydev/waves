6fb6cad7f3e9f6ffacb4db645c54f3b07827bf4e
neillycubed <neillybusiness3@gmail.com>
2021-09-24 04:53:13 -0700

    change api function handling for Post Dropdown

--- src/components/PostDropdown/PostDropdown.tsx
+++ src/components/PostDropdown/PostDropdown.tsx
@@ -1,20 +1,31 @@
 import React, { useState, useContext } from 'react';
 
+import { LoadingContext } from '../contexts/LoadingContext';
+
+import { handleDeletePost } from '../../api/PostAPI';
+
 import TrashSVG from '../../svg/trash.svg';
 
 require('./PostDropdown.css');
 
 type PostDropdownProps = {
-    handleDeletePost: () => void;
+    post_id: number;
+    handleLoadState: (loadState: boolean) => void;
 };
 
-const PostDropdown = ({ handleDeletePost }: PostDropdownProps) => {
-
+const PostDropdown = ({ post_id, handleLoadState }: PostDropdownProps) => {
+    const { load_state, loading_dispatch } = useContext(LoadingContext);
     return (
         <div className="postDropdownContainer">
             <div className="postDropdownWrapper">
                 <span className="postDropdownItem" onClick={() => {
-                    handleDeletePost();
+                    loading_dispatch({ loading: true, type: 'loading_bar' });
+                    handleLoadState(true);
+                    handleDeletePost(post_id).then(() => {
+                        handleLoadState(false);
+                        loading_dispatch({ loading: true, type: 'bar' });
+                    }).catch(() => loading_dispatch({ loading: true, type: 'bar' }));
+
                 }}>
                     <TrashSVG />
                     Delete post
